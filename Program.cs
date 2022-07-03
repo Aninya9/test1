e3349bc581d0ec1d25f24b395a8d3eca6a646a06
Anna <aninya9@gmail.com>
2022-07-03 16:51:13 +0300

    Second commit

--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 char[] a = arr[0].ToCharArray();
 int n = arr[0].Length;
 if (n < 4) Console.WriteLine(arr[0]);
+
+char[] b = arr[0].ToCharArray();
+int m = arr[1].Length;
+if (m < 4) Console.WriteLine(arr[1]);
